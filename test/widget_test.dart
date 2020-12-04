05986c4113c4d0926e4405653ee27f66e95e3d4f
Yu Soof <fuzuyx@gmail.com>
2020-12-04 09:32:43 +0700

    feat(arch): :building_construction: Hello world with riverpod
    We downgrade the Dart SDK version to disable "sound null safety",
    as the package we'll add doesn't support it yet. Then, adds the
    flutter_riverpod package. Import it, wrap entire app with
    "ProviderScope". Create a view with the provider from riverpod package.
    Write tests for it, then you have it!

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,22 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:quest/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets(
+    'The "Xaltius Quest" is exist',
+    (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      //
+      // We declare "ProviderScope" as riverpod requirements,
+      // and follows the runApp method for the tests to pass.
+      await tester.pumpWidget(ProviderScope(child: QuestApp()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+      // Verify that our "hello app" has "Xaltius Quest".
+      expect(find.text('Xaltius Quest'), findsOneWidget);
+    },
+  );
 }
